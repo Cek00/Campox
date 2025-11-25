e0768a7ffd5503d4df1d641befdb1edb0e5ce88a
angie <angiesierra240@gmail.com>
2025-11-24 22:59:55 -0500

    ajustes payment

--- back/src/payment/payment.repository.ts
+++ back/src/payment/payment.repository.ts
@@ -7,45 +7,54 @@ import { UpdatePaymentDto } from './Dtos/updatePayment.dto';
 
 @Injectable()
 export class PaymentRepository {
-    constructor(
-        @InjectRepository(PaymentEntity)
-        private readonly paymentDB: Repository<PaymentEntity>,
-    ) {}
+  constructor(
+    @InjectRepository(PaymentEntity)
+    private readonly paymentDB: Repository<PaymentEntity>,
+  ) {}
 
-    async getAllPaymentsRepository() {
-        return await this.paymentDB.find();
-    }
+  //metodo para obtener todos los pagos
+  getAllPaymentsRepository() {
+    return this.paymentDB.find();
+  }
 
-    async getPaymentByIdRepository(uuid: string) {
-        return await this.paymentDB.findOne({ where: { uuid } });
-    }
+  //metodo para obtener el pago por su id
+  getPaymentByIdRepository(uuid: string) {
+    return this.paymentDB.findOne({ where: { uuid } });
+  }
 
-    async createPaymentRepository(data: CreatePaymentDto) {
-        const newPayment = this.paymentDB.create({
-            amount: data.amount,
-            paymentmethod: data.paymentmethod,
-            status: data.status,
-            paymentDate: data.paymentDate,
-        });
+  //metodo para crear un nuevo pago
+  async createPaymentRepository(data: CreatePaymentDto) {
+    const newPayment = this.paymentDB.create({
+      amount: data.amount,
+      paymentmethod: data.paymentmethod,
+      status: data.status,
+      paymentDate: new Date(data.paymentDate),
+    });
 
-        await this.paymentDB.save(newPayment);
+    await this.paymentDB.save(newPayment);
 
-        return { message: 'El pago ha sido creado exitosamente.' };
-    }
+    return { message: 'El pago ha sido creado exitosamente.' };
+  }
 
-    async updatePaymentRepository(paymentExisting: PaymentEntity, data: UpdatePaymentDto) {
-        if (data.amount) paymentExisting.amount = data.amount;
-        if (data.paymentmethod) paymentExisting.paymentmethod = data.paymentmethod;
-        if (data.status) paymentExisting.status = data.status;
-        if (data.paymentDate) paymentExisting.paymentDate = data.paymentDate;
+  //metodo para actualizar un pago
+  async updatePaymentRepository(
+    paymentExisting: PaymentEntity,
+    data: UpdatePaymentDto,
+  ) {
+    if (data.amount !== undefined) paymentExisting.amount = data.amount;
+    if (data.paymentmethod) paymentExisting.paymentmethod = data.paymentmethod;
+    if (data.status) paymentExisting.status = data.status;
+    if (data.paymentDate)
+      paymentExisting.paymentDate = new Date(data.paymentDate);
 
-        await this.paymentDB.save(paymentExisting);
+    await this.paymentDB.save(paymentExisting);
 
-        return { message: 'Pago actualizado correctamente.' };
-    }
+    return { message: 'Pago actualizado correctamente.' };
+  }
 
-    async deletePaymentRepository(paymentExisting: PaymentEntity) {
-        await this.paymentDB.remove(paymentExisting);
-        return { message: 'Pago eliminado exitosamente.' };
-    }
+  //metodo para eliminar un pago
+  async deletePaymentRepository(paymentExisting: PaymentEntity) {
+    await this.paymentDB.remove(paymentExisting);
+    return { message: 'Pago eliminado exitosamente.' };
+  }
 }
